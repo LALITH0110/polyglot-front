4e3e76d1c1c37c33c811ec7a9417d94c98a679f5
LALITH0110 <lalith.kothuru@gmail.com>
2025-08-29 14:38:07 -0500

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 import { Toaster } from 'sonner'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
   title: 'Polyglot File Generator',
@@ -29,6 +30,7 @@ html {
       <body>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
